3876789491fad3133779ccdd33e5624a762ddaa9
aFurkansalti <ahmetfurkansalti@gmail.com>
2022-06-05 17:46:28 +0300

    Pratik 3 added

---
+++ 6 - Diziler/pratik 3/Pratik3.java
@@ -0,0 +1,20 @@
+public class Pratik3 {
+    public static void main(String[] args) {
+        String[][] matrix = new String[7][4];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if ((row == 0) || (row == 3) || (row == 6)) {
+                    matrix[row][col] = " * ";
+                } else if ((col == 0) || (col == 3)) {
+                    matrix[row][col] = " * ";
+                } else {
+                    matrix[row][col] = "   ";
+                }
+
+                System.out.print(matrix[row][col]);
+            }
+            System.out.println();
+        }
+    }
+}
